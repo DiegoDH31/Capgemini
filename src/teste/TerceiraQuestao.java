f32be88649d257f24d790aa5d6b3aace69a7f86b
Diego Henrique de Morais Pontes <dh_henrique_21@hotmail.com>
2022-02-20 10:56:45 -0300

    Impressões no console para guia removidos

--- src/teste/TerceiraQuestao.java
+++ src/teste/TerceiraQuestao.java
@@ -7,6 +7,7 @@ public class TerceiraQuestao {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Insira a palavra desejada: ");
 		String anagram = scan.nextLine();
 		int contador = 0;
 		//boolean match = false;
@@ -14,20 +15,15 @@ public class TerceiraQuestao {
 		StringBuilder sb = new StringBuilder();
 		sb.append(anagram);
 		
-		System.out.println(sb.length());
-	
 		//verificar quantos pares de letras iguais
 		try {
 			for (int i = 0; i < sb.length(); i++) {
 				String aux = String.valueOf(sb.charAt(i));
-				System.out.println("aux " + aux);
 				
 				for (int j = 0; j < (sb.length() - 1); j++) {
 					String aux2 = String.valueOf(sb.charAt(j+1));
-					System.out.println("aux2 " + aux2);
 					
 					if(aux.equals(aux2)) {
-						System.out.println("igualou = " + aux + " com " + aux2);
 						contador++;
 						sb.deleteCharAt(i);
 						sb.deleteCharAt(j+1);
@@ -41,7 +37,8 @@ public class TerceiraQuestao {
 			
 		}
 		
-		System.out.println("pares - " + contador);
+		//Imprimindo na tela os pares encontrados
+		System.out.println(contador);
 		
 		scan.close();
 	}
